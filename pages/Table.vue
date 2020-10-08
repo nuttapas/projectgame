4f4068e85967bc9b7aabd27e879591ab977d622a
nuttapas <68525325+nuttapas@users.noreply.github.com>
2020-10-08 13:51:50 +0700

    edit

---
+++ pages/Table.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title>
+        <v-spacer></v-spacer>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="listdata"
+        :items-per-page="5"
+        :search="search"
+        class="elevation-1"
+      ></v-data-table>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'username',
+          align: 'start',
+          sortable: false,
+          value: 'username',
+        },
+        {
+          text: 'tagname',
+          value: 'tagname',
+        },
+        {
+          text: 'email',
+          value: 'email',
+        },
+        {
+          text: 'pice',
+          value: 'pice',
+        },
+      ],
+    }
+  },
+  computed: {
+    listdata: {
+      get() {
+        return this.$nuxt.$store.state.list
+      },
+    },
+  },
+}
+</script>
